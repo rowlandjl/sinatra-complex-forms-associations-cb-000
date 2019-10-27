0045671f62027ce1551722979f3b3a71b0875175
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-27 18:38:59 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -38,7 +38,7 @@ class PetsController < ApplicationController
     @pet.update(params[:pet])
     
     if !params["owner"]["name"].empty?
-      @pet.owner = Pet.create(name: params["owner"]["name"])
+      @pet.owner = Owner.create(name: params["owner"]["name"])
     end 
     redirect to "pets/#{@pet.id}"
   end
