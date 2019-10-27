27ac77998f1ee39d0bbc4def900abd13c02c40db
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-27 18:32:52 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,29 +1,51 @@
 class OwnersController < ApplicationController
 
+  # index 
   get '/owners' do
     @owners = Owner.all
     erb :'/owners/index' 
   end
-
+  
+  # new 
   get '/owners/new' do 
+    @pets = Pet.all 
     erb :'/owners/new'
   end
 
   post '/owners' do 
+    @owner = Owner.create(params[:owner])
     
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end 
+    redirect "/owners/#{@owner.id}"
   end
-
-  get '/owners/:id/edit' do 
-    @owner = Owner.find(params[:id])
-    erb :'/owners/edit'
-  end
-
+  
+  # show 
   get '/owners/:id' do 
     @owner = Owner.find(params[:id])
     erb :'/owners/show'
   end
 
+  # edit 
+  get '/owners/:id/edit' do 
+    @owner = Owner.find(params[:id])
+    @pets = Pet.all 
+    erb :'/owners/edit'
+  end
+
   patch '/owners/:id' do 
-   
+    if !params[:owner].keys.include?("pet_ids")
+      params[:owner]["pet_ids"] = []
+    end 
+    
+    @owner = Owner.find(params[:id])
+    @owner.update(params["owner"])
+    
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end 
+    
+    redirect "owners/#{@owner.id}"
   end
 end
